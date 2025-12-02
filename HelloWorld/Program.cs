92ee0344cdb031048f15bd6de6782009b37348b2
pchp250g8-home <pchp250g8@tutamail.com>
2025-12-02 17:34:38 +0300

    Добавьте файлы проекта.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.CursorVisible = false;
+            Console.CursorTop = 10;
+            Console.CursorLeft = 10;
+            Console.Write("Hello,World!!!");
+            Console.ReadKey();
+        }
+    }
+}
